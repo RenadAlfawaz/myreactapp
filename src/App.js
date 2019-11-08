e4704dedec942982d1de78b1f0ca90f93bcb125b
Mohamed Sherif Abdel Aziz <m7sherif@hotmail.com>
2019-11-08 22:28:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [result, setResult] = useState(0);
 
   const addButton = () => {
-    axios.get('http://184.173.1.252:30805/?num1=' + num1 + '&num2=' + num2).then((result) => {
+    axios.get('http://184.173.1.252:32205/?num1=' + num1 + '&num2=' + num2).then((result) => {
       console.log(result.data);
       setResult(result.data);
     }).catch((error) => {
